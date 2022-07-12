58995ee7553896f0cf3892b3d85d59cf0e246b73
uniqueadh56 <95867657+uniqueadh56@users.noreply.github.com>
2022-07-12 08:57:05 +0545

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const server = express()
+server.all("/", (req, res) => {
+  res.send("server is running!")
+})
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log("Server is ready.")
+  })
+}
+
+module.exports = keepAlive
